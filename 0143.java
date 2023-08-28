2e074ad1e13fcdc5271e73d3d79dbdcd01805047
naixuedecha450 <142955155+naixuedecha450@users.noreply.github.com>
2023-08-28 14:54:03 +0800

    Create 0143.java

---
+++ 0143.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortList {
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("Java");
+    list.add("Python");
+    list.add("C++");
+    System.out.println(list);
+    Collections.sort(list);
+    System.out.println(list);
+  }
+}
